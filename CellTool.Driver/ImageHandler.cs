c8c6695cec71b7716542cf5476803af073702d65
Tommy Durant <thomas.durant@yale.edu>
2015-12-02 10:26:54 -0500

    Revert "imported images as byte array to database, but it timed out and only did about 300. Will try again, but first going to try and load/view image from MVC"
    This reverts commit ea6023b0de177972669dabcbfb0932d4ecc13d97.

--- CellTool.Driver/ImageHandler.cs
+++ CellTool.Driver/ImageHandler.cs
@@ -52,7 +52,6 @@ namespace CellTool.Driver
 
         public void BulkImportImages()
         {
-
             string sourceDir = "C:/Users/thoma/Documents/00GitHub/00_LOCAL_ONLY/Symmetric_Rbc_Crop/";
 
             string[] fileEntries = Directory.GetFiles(sourceDir);
@@ -60,12 +59,7 @@ namespace CellTool.Driver
             Console.WriteLine(sourceDir);
             foreach (string line in fileEntries)
             {
-                //Console.WriteLine(line);
-                byte[] arr;
-                Image img = Image.FromFile(line);
-                arr = imageToByteArray(img);
-
-                SqlBulkImport(arr);
+                Console.WriteLine(line);
             }
 
 
