b7d0b68c8d625b6467869959643fd3b443c9fe4b
Tommy Durant <thomas.durant@yale.edu>
2016-01-18 14:35:34 -0500

    html helper added

---
+++ Images.Web/Content/htmlhelpers/CustomHtmlHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// added this namespace to web.config
+namespace Images.Web.Content.htmlhelpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper, string src)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+
+    }
+}
