ffb2b65f05daa56c7da75d56bb7c0d662ccdb41e
Tommy Durant <thomas.durant@yale.edu>
2016-01-18 15:23:56 -0500

    able to display images from path DB

--- Images.Web/Content/htmlhelpers/CustomHtmlHelpers.cs
+++ Images.Web/Content/htmlhelpers/CustomHtmlHelpers.cs
@@ -9,10 +9,16 @@ namespace Images.Web.Content.htmlhelpers
 {
     public static class CustomHtmlHelpers
     {
+        // https://www.youtube.com/watch?v=2mACi5D739g
         public static IHtmlString Image(this HtmlHelper helper, string src)
         {
+
             TagBuilder tb = new TagBuilder("img");
+
+            // relative path to url conversion
             tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+
+            // returns a string wrapped in html string; 
             return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
         }
 
--- Images.Web/Controllers/LabelController.cs
+++ Images.Web/Controllers/LabelController.cs
@@ -6,9 +6,12 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Images.Web.Controllers
 {
@@ -16,7 +19,6 @@ namespace Images.Web.Controllers
     {
         // instantiate Db connection class; CellToolContext which relies on the connection
         // string in the Web.Config file. 
-        
 
         SqlCommand cmd;
         SqlConnection con;
@@ -33,84 +35,23 @@ namespace Images.Web.Controllers
 
         public ActionResult Index()
         {
+            ImageHandler ih = new ImageHandler();
+            MemoryStream ms = new MemoryStream();
+
             var query = (from c in _db.OriginalCellData select new { c.ImagePath }).First();
 
-            var ImPath = query.ImagePath;
+            var imageRelativePath = query.ImagePath;
+            var imageOpen = Image.FromFile(imageRelativePath);
 
-            //var imArray = _db.OriginalCellData.First<CellGroup>(a => a.ImageBin);
-            //string imageBase64Data = Convert.ToBase64String(imArray);
-            //string imageDataURL = string.Format("http://localhost/{0}", ImPath);
+            imageOpen.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            var imArray = ms.ToArray();
 
-            ViewBag.ImageData = ImPath;
+
+            string imageBase64Data = Convert.ToBase64String(imArray);
+            string imageDataURL = string.Format("data:image/png;base64,{0}", imageBase64Data);
+            ViewBag.ImageData = imageDataURL;
 
             return View();
         }
-
-        public ActionResult GetImage()
-        {
-            string path = Server.MapPath("~/images/computer.png");
-            byte[] imageByteData = System.IO.File.ReadAllBytes(path);
-            return File(imageByteData, "image/png");
-        }
-
-        //public ActionResult Index()
-        //{
-
-        //    /*"There's no better thing than Inversion of Control and Dependency Injection, 
-        //    generic specialization, the decorator pattern, chains of responsibilities, 
-        //    and extensible software."     */
-
-
-        //    //GetCellImage cellData = new GetCellImage();
-
-
-        //    string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=CellData;Integrated Security=True;MultipleActiveResultSets=True";
-
-        //    // establish database connection
-        //    SqlConnection objConn = new SqlConnection(connectionString);
-        //    objConn.Open();
-
-        //    // establish link (adapter) between database and dataset object
-        //    SqlDataAdapter daCellData = new SqlDataAdapter("Select * From Authors", objConn);
-
-        //    /* You must declare and create an instance of a DataSet object, at which time 
-        //    you can supply a name for the entire DataSet before you can start to load any 
-        //    data.The name may contain several distinct tables. */
-        //    //DataSet dsCellDatas = new DataSet("CellDatas");
-
-        //    /* The SqlDataAdapter class provides two methods, Fill and FillSchema, that are 
-        //    crucial to loading this data. Both of these methods load information into a DataSet. 
-        //    Fill loads the data itself, and FillSchema loads all of the available metadata about 
-        //    a particular table (such as column names, primary keys, and constraints). A good way 
-        //    to handle the data loading is to run FillSchema followed by Fill. For example: */
-        //    //daCellData.FillSchema(dsCellDatas, SchemaType.Source, "CellDatas");
-        //    //daCellData.Fill(dsCellDatas, "CellDatas");
-
-        //    /* The data is now available as an individual DataTable object within the Tables 
-        //    collection of the DataSet. If you specified a table name in the calls to FillSchema
-        //    and Fill, you can use that name to access the specific table that you want.*/
-        //    //DataTable tblCelldatas;
-        //    //tblCelldatas = dsCellDatas.Tables["CellDatas"];
-
-        //    //foreach (DataRow drCurrent in tblAuthors.Rows)
-        //    //{
-        //    //    Console.WriteLine("{0} {1}",
-        //    //        drCurrent["au_fname"].ToString(),
-        //    //        drCurrent["au_lname"].ToString());
-        //    //}
-        //    //Console.ReadLine();
-
-        //    return View();
-        //}
-        ////public PartialViewResult DislpayImage(int id)
-        ////{
-        ////    DataSet dsa = new DataSet();
-
-        ////    dsa = objImage.getUserImage(id);
-        ////    var imagedata = dsa.Tables[0].Columns["ImageBin"];
-        ////    var f = File(imagedata, "image/png");
-
-        ////    return PartialView(f);
-        ////}
     }
 }   
